26ef7aa6815a137a451e285ae60ad35c5bd16169
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-10-16 18:31:01 +0700

    add tests

--- mb_project/posts/tests.py
+++ mb_project/posts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='Just a test.')
+    
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'Just a test.')
