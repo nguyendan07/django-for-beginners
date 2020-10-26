db70109ba28cd97ba1972f2f9621439771f9d6e7
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-10-26 23:59:36 +0700

    add test homepage

--- mb_project/posts/tests.py
+++ mb_project/posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Post
 
@@ -11,3 +12,21 @@ class PostModelTest(TestCase):
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'Just a test.')
+
+
+class HomePageTestView(TestCase):
+    def setUp(self):
+        Post.objects.create(text='This is another test')
+    
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_view_user_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
