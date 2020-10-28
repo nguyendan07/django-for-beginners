3a1a406a024457f95b8f25751c3f1a405b34ca17
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-10-28 17:44:53 +0700

    Individual blog pages

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('posts/', BlogListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail')
 ]
