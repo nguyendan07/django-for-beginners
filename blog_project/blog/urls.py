ff2bf6b9b8b4a202872d27a9f60d70923563a2e9
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-10-30 18:49:48 +0700

    add forms create new post

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('posts/', BlogListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail')
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new')
 ]
