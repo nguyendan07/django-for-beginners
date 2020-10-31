0cc2acb2a69efbce12002db4afcb34b3d05f4b0a
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-10-31 19:08:09 +0700

    add form confirm delete post

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete')
 ]
